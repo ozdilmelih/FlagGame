39fcb5c29553e4ae2ebfac983f148ac65f94f676
ozdilmelih <ozdilmelih@gmail.com>
2024-03-20 16:39:31 +0300

    Initial Commit

---
+++ FlagGame/FlagGameApp.swift
@@ -0,0 +1,17 @@
+//
+//  FlagGameApp.swift
+//  FlagGame
+//
+//  Created by melih özdil on 18.03.2024.
+//
+
+import SwiftUI
+
+@main
+struct FlagGameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
